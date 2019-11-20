0b5d80baf317f70684a665ea1e872a69d5d34492
hetiantian <969795191@qq.com>
2019-11-20 14:03:05 +0800

    add IO Block and Synchronize Block

--- src/main/java/com/h2t/study/concurrent/interrupt/IOBlock.java
+++ src/main/java/com/h2t/study/concurrent/interrupt/IOBlock.java
@@ -1,5 +1,8 @@
 package com.h2t.study.concurrent.interrupt;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * IO阻塞
  *
@@ -8,4 +11,33 @@ package com.h2t.study.concurrent.interrupt;
  * @Date 2019/11/19 17:57
  */
 public class IOBlock {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Task(System.in));  //输入流的来源来自用户输入
+        thread.start();
+        Thread.sleep(3 * 1000);  //睡眠3s
+        //thread.interrupt();
+        //System.out.println("是否被中断：" + thread.isInterrupted());
+        //System.out.println("中断已执行");
+    }
+
+    public static class Task implements Runnable {
+        private InputStream inputStream;
+
+        public Task(InputStream inputStream) {
+            this.inputStream = inputStream;
+        }
+
+        /**
+         * 自定义任务
+         */
+        @Override
+        public void run() {
+            try {
+                inputStream.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("End...");
+        }
+    }
 }
--- src/main/java/com/h2t/study/concurrent/interrupt/SynchronizedBlock.java
+++ src/main/java/com/h2t/study/concurrent/interrupt/SynchronizedBlock.java
@@ -7,38 +7,33 @@ package com.h2t.study.concurrent.interrupt;
  * @version 1.0
  * @Date 2019/11/19 17:56
  */
-public class SynchronizedBlock implements Runnable {
-
-    /**
-     * 构造函数
-     */
-    public SynchronizedBlock() {
-        new Thread() {
-            public void run() {
-                f();
-            }
-        }.start();
-    }
-
+public class SynchronizedBlock {
     public static void main(String[] args) throws InterruptedException {
-        SynchronizedBlock synchronizedBlock = new SynchronizedBlock();
-        Thread thread = new Thread(synchronizedBlock);
+        Thread thread = new Thread(new Task());
         thread.start();
         Thread.sleep(3 * 1000);  //执行三秒后中断线程
         thread.interrupt();
+        System.out.println(thread.isInterrupted());
     }
 
-    public synchronized void f() {
-        int i = 0;
-        while (true) {
-            System.out.println(i++);
+    public static class Task implements Runnable {
+        public Task() {
+            new Thread() {
+                public void run() {
+                    f();
+                }
+            }.start();
+        }
+
+        public synchronized void f() {
+            while (true) {
+            }
+        }
+
+        @Override
+        public void run() {
+            f();
+            System.out.println("End");
         }
     }
-
-    @Override
-    public void run() {
-        f();
-        System.out.println("End");
-    }
-
 }
